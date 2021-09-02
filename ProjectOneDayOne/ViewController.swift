89c83c536f5d91a0009684b8de127f68fb8bc1cb
Zsofia Nagy <nagy.zso.94@gmail.com>
2021-09-02 08:39:52 +0200

    Initial Commit

---
+++ ProjectOneDayOne/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectOneDayOne
+//
+//  Created by Nagy Zsófia on 2021. 09. 02..
+//  Copyright © 2021. Nagy Zsófia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
